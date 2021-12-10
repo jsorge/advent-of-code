3e0b9faf37ead318bc5f8a766a579cef857012a9
Jared Sorge <jared@jsorge.net>
2021-12-09 22:10:57 -0800

    Day 10

--- Years/2021/Sources/Day10.swift
+++ Years/2021/Sources/Day10.swift
@@ -8,10 +8,112 @@ import AOCCore
 
 final class Day10: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let lines = input.toLines()
+
+        var corruptingChars = [ChunkClose]()
+        for line in lines {
+            var openers = [ChunkOpen]()
+            for char in line {
+                if let open = ChunkOpen(character: char) {
+                    openers.append(open)
+                } else if let close = ChunkClose(character: char) {
+                    let lastOpen = openers.popLast()
+                    if lastOpen?.closing != close {
+                        corruptingChars.append(close)
+                        break
+                    }
+                }
+            }
+        }
+
+        let totalValue = corruptingChars.map({ $0.syntaxCheckerValue }).reduce(0, +)
+        return totalValue
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let lines = input.toLines()
+
+        var incompleteLineScores = [Int]()
+        for line in lines {
+            var openers = [ChunkOpen]()
+            var corrupted = false
+            for char in line {
+                if let open = ChunkOpen(character: char) {
+                    openers.append(open)
+                } else if let close = ChunkClose(character: char) {
+                    let lastOpen = openers.popLast()
+                    if lastOpen?.closing != close {
+                        corrupted = true
+                        break
+                    }
+                }
+            }
+
+            guard corrupted == false, openers.count > 0 else { continue }
+            let lineValue = openers
+                .reversed()
+                .map({ $0.closing })
+                .reduce(into: 0) { partialResult, close in
+                    partialResult *= 5
+                    partialResult += close.autocompleteValue
+                }
+
+            incompleteLineScores.append(lineValue)
+        }
+
+        incompleteLineScores.sort()
+        let midIndex = (incompleteLineScores.count - 1) / 2
+        return incompleteLineScores[midIndex]
+    }
+}
+
+private enum ChunkOpen: String {
+    case paren = "("
+    case bracket = "["
+    case brace = "{"
+    case angle = "<"
+
+    var closing: ChunkClose {
+        switch self {
+        case .paren: return .paren
+        case .bracket: return .bracket
+        case .brace: return .brace
+        case .angle: return .angle
+        }
+    }
+
+    init?(character: Character) {
+        guard let open = Self.init(rawValue: String(character)) else { return nil }
+        self = open
+    }
+}
+
+private enum ChunkClose: String {
+    case paren = ")"
+    case bracket = "]"
+    case brace = "}"
+    case angle = ">"
+
+    var syntaxCheckerValue: Int {
+        switch self {
+        case .paren: return 3
+        case .bracket: return 57
+        case .brace: return 1197
+        case .angle: return 25137
+        }
+    }
+
+    var autocompleteValue: Int {
+        switch self {
+        case .paren: return 1
+        case .bracket: return 2
+        case .brace: return 3
+        case .angle: return 4
+        }
+    }
+
+    init?(character: Character) {
+        guard let close = Self.init(rawValue: String(character)) else { return nil }
+        self = close
     }
 }
--- Years/2021/Tests/Day10Tests.swift
+++ Years/2021/Tests/Day10Tests.swift
@@ -5,16 +5,38 @@
 
 import XCTest
 @testable import AOC2021
+import TestingSupport
+
+private let sampleInput = """
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+"""
 
 class Day10Tests: XCTestCase {
     let day = Day10()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(26397, day.part1(sampleInput) as? Int)
+
+        let inputFromFile = try Loader.loadDay("10", from: .module)
+        let result = day.part1(inputFromFile)
+        printResult(result)
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(288957, day.part2(sampleInput) as? Int)
+
+        let inputFromFile = try Loader.loadDay("10", from: .module)
+        let result = day.part2(inputFromFile)
+        printResult(result)
     }
 
 }
