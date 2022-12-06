0883e1aadd176f56f58a7d68b90ef67c217904ad
Jared Sorge <jared@jsorge.net>
2022-12-05 19:55:43 -0800

    2015 day 2

--- Years/2015/Day2/Day2.swift
+++ Years/2015/Day2/Day2.swift
@@ -10,10 +10,60 @@ import AOCCore
 
 final class Day2: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        return input
+            .toLines(omittingEmpties: true)
+            .map { Box(string: $0) }
+            .map {
+                $0.surfaceArea + $0.smallestSideArea
+            }
+            .reduce(0, +)
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        return input
+            .toLines(omittingEmpties: true)
+            .map { Box(string: $0) }
+            .map { box -> Int in
+                (box.smallestSide.0 * 2) + (box.smallestSide.1 * 2) + box.volume
+            }
+            .reduce(0, +)
+    }
+}
+
+private struct Box {
+    let length: Int
+    let width: Int
+    let height: Int
+
+    var surfaceArea: Int {
+        (2 * length * width) + (2 * width * height) + (2 * height * length)
+    }
+
+    var smallestSide: (Int, Int) {
+        let shortestSides = [length, width, height]
+            .sorted()
+            .prefix(2)
+
+        return (shortestSides.first!, shortestSides.last!)
+    }
+
+    var volume: Int {
+        length * width * height
+    }
+
+    var smallestSideArea: Int {
+        let c1 = length * width
+        let c2 = length * height
+        let c3 = width * height
+
+        return min(min(c1, c2), c3)
+    }
+
+    init(string: String) {
+        let regex = /(\d+)x(\d+)x(\d+)/
+        let match = try! regex.firstMatch(in: string)!
+        self.length = Int(match.output.1)!
+        self.width = Int(match.output.2)!
+        self.height = Int(match.output.3)!
     }
 }
--- Years/2015/Day2/Day2Tests.swift
+++ Years/2015/Day2/Day2Tests.swift
@@ -8,13 +8,14 @@ import XCTest
 import TestingSupport
 
 private let sampleInput = """
+2x3x4
 """
 
 class Day2Tests: XCTestCase {
     let day = Day2()
 
     func testPart1() throws {
-        XCTAssertEqual(0, day.part1(sampleInput) as? Int)
+        XCTAssertEqual(58, day.part1(sampleInput) as? Int)
 
         let inputFromFile = try Loader.loadDay("2", from: .module)
         let result = day.part1(inputFromFile)
@@ -22,7 +23,7 @@ class Day2Tests: XCTestCase {
     }
 
     func testPart2() throws {
-        XCTAssertEqual(0, day.part2(sampleInput) as? Int)
+        XCTAssertEqual(34, day.part2(sampleInput) as? Int)
 
         let inputFromFile = try Loader.loadDay("2", from: .module)
         let result = day.part2(inputFromFile)
